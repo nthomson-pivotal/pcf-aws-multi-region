1dc0ae13629ab25ed9c5d4ef06673e1e2b603551
Niall Thomson <nthomson@pivotal.io>
2018-09-17 12:30:10 -0700

    Removed bad A record resource, used apps domain outputs from paasify module for weighted DNS entry

--- dns.tf
+++ dns.tf
@@ -2,14 +2,6 @@ data "aws_route53_zone" "selected" {
   name         = "${var.dns_suffix}."
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "spring-music"
-  type    = "A"
-  ttl     = "300"
-  records = ["10.0.0.1"]
-}
-
 resource "aws_route53_record" "pcf-1" {
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "spring-music"
@@ -21,7 +13,7 @@ resource "aws_route53_record" "pcf-1" {
   }
 
   set_identifier = "pcf-use1"
-  records        = ["spring-music.apps.mr-use1.${var.dns_suffix}"]
+  records        = ["spring-music.${module.pcf-1.apps_domain}"]
 }
 
 resource "aws_route53_record" "pcf-2" {
@@ -35,5 +27,5 @@ resource "aws_route53_record" "pcf-2" {
   }
 
   set_identifier = "pcf-usw2"
-  records        = ["spring-music.apps.mr-usw2.${var.dns_suffix}"]
+  records        = ["spring-music.${module.pcf-2.apps_domain}"]
 }
